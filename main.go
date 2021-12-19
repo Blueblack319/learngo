6574184c3ec99e4660bde41570c5dba74e9afc37
Yang Jae-Hoon <didwogns789@yonsei.ac.kr>
2021-12-19 11:12:10 +0000

    go routine

--- main.go
+++ main.go
@@ -2,37 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"time"
 )
 
 func main() {
-	var results = map[string]bool{}
-	urls := []string{"https://www.google.com",
-		"https://www.youtube.com",
-		"https://www.facebook.com",
-		"https://www.twitter.com",
-		"https://www.instagram.com",
-		"https://www.baidu.com",
-		"https://www.wikipedia.org",
-		"https://www.yandex.ru",
-		"https://www.yahoo.com",
-		"https://www.amazon.com",
-		"https://www.netflix.com",
-		"https://www.live.com",
-		"https://www.zoom.us"}
-	for _, url := range urls {
-		checkURL(url, results)
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go whoIsSexy("Hoon")
+	whoIsSexy("Aiden")
 }
 
-func checkURL(url string, results map[string]bool) {
-	fmt.Println("Checking :", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		results[url] = false
+func whoIsSexy(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(name, "is sexy.", i)
+		time.Sleep(time.Second)
 	}
-	results[url] = true
 }
