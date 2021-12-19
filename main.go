eb87235a216732e498e0d4437d39f5d959eeb6b3
Yang Jae-Hoon <didwogns789@yonsei.ac.kr>
2021-12-19 03:37:49 +0000

    for range, for ;, ignore return var

--- main.go
+++ main.go
@@ -16,7 +16,22 @@ func multiArgs(strings ...string) []string {
 	return strings
 }
 
+func forExampleLikeC(numbers ...int) {
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(i, numbers[i])
+	}
+}
+
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
 func main() {
-	leng, _ := lenAndUpper("James")
-	fmt.Println(leng)
+	forExampleLikeC(1, 2, 3, 4, 5, 6)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
