816a912a9f7d2ec9b2b991994a066e9238395218
Yang Jae-Hoon <didwogns789@yonsei.ac.kr>
2021-12-23 10:20:53 +0000

    play with git

--- main.go
+++ main.go
@@ -2,37 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"time"
 )
 
 func main() {
-	var results = map[string]bool{}
-	urls := []string{"https://www.google.com",
-		"https://www.youtube.com",
-		"https://www.facebook.com",
-		"https://www.twitter.com",
-		"https://www.instagram.com",
-		"https://www.baidu.com",
-		"https://www.wikipedia.org",
-		"https://www.yandex.ru",
-		"https://www.yahoo.com",
-		"https://www.amazon.com",
-		"https://www.netflix.com",
-		"https://www.live.com",
-		"https://www.zoom.us"}
-	for _, url := range urls {
-		checkURL(url, results)
+	c := make(chan string)
+	people := []string{"hyang", "gwang", "hoon", "crazybirdz", "crossbeam"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(people); i++ {
+		fmt.Println("Waiting for message ", i)
+		fmt.Println(<-c)
 	}
 }
 
-func checkURL(url string, results map[string]bool) {
-	fmt.Println("Checking :", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		results[url] = false
-	}
-	results[url] = true
+func isSexy(name string, c chan string) {
+	time.Sleep(time.Second * 2)
+	c <- name + " is sexy"
 }
